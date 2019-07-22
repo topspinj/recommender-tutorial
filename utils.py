e05567c751dc320fd45c1b7c0dd21a1bce1201ea
Jill Cates <cates.jill@gmail.com>
2019-07-22 18:23:33 -0400

    Add find_similar_movies function to utils

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ Utils
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
 
 def create_X(df):
     """
@@ -36,3 +37,32 @@ def create_X(df):
     X = csr_matrix((df["rating"], (item_index, user_index)), shape=(M, N))
     
     return X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper
+
+def find_similar_movies(movie_id, X, k, metric='cosine', show_distance=False):
+    """
+    Finds k-nearest neighbours for a given movie id.
+    
+    Args:
+        movie_id: id of the movie of interest
+        X: user-item utility matrix
+        k: number of similar movies to retrieve
+        metric: distance metric for kNN calculations
+    
+    Returns:
+        list of k similar movie ID's
+    """
+    neighbour_ids = []
+    
+    movie_ind = movie_mapper[movie_id]
+    movie_vec = X[movie_ind]
+    k+=1
+    kNN = NearestNeighbors(n_neighbors=k, algorithm="brute", metric=metric)
+    kNN.fit(X)
+    if isinstance(movie_vec, (np.ndarray)):
+        movie_vec = movie_vec.reshape(1,-1)
+    neighbour = kNN.kneighbors(movie_vec, return_distance=show_distance)
+    for i in range(0,k):
+        n = neighbour.item(i)
+        neighbour_ids.append(movie_inv_mapper[n])
+    neighbour_ids.pop(0)
+    return neighbour_ids
